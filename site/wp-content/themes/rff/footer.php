4aad7dd49a82ae536401efa344897a625c12ab76
Alex Morega <alex@grep.ro>
2014-03-13 18:56:10 +0200

    minimal theme for rff.ro

---
+++ site/wp-content/themes/rff/footer.php
@@ -0,0 +1,14 @@
+      <?php get_sidebar(); ?>
+    </div>
+
+    <div class="footer">
+    </div>
+
+    <!-- Page generated:
+      <?php timer_stop(1); ?> s,
+      <?php echo get_num_queries(); ?> queries
+    -->
+
+    <?php wp_footer(); ?>
+  </body>
+</html>
---
+++ site/wp-content/themes/rff/header.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <title><?php wp_title('|', true, 'right'); ?></title>
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css"
+          rel="stylesheet">
+    <link href="<?php echo get_stylesheet_directory_uri(); ?>/style.css"
+          rel="stylesheet">
+    <meta http-equiv="Content-language"
+          content="<?php bloginfo('language'); ?>">
+    <?php wp_head(); ?>
+  </head>
+
+  <body <?php body_class(); ?>>
+    <div class="container">
+
+      <?php wp_nav_menu(array(
+        'theme_location' => 'top_menu',
+        'depth' => 1,
+        'container' => 'div',
+        'container_class' => 'menu',
+        'menu_id' => false,
+        'menu_class' => false,
+        'fallback_cb' => false,
+        )); ?>
+
+      <?php wp_nav_menu(array(
+        'theme_location' => 'navigation',
+        'depth' => 2,
+        'container' => 'div',
+        'container_class' => 'nav',
+        'menu_class' => 'dd',
+        'menu_id' => 'dd',
+        //'walker' => new extended_walker(),
+        'fallback_cb' => false,
+        )); ?>
