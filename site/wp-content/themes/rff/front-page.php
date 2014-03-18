aff0087f8b6061ce09de5a697a31f6694a7e3825
Alex Morega <alex@grep.ro>
2014-03-18 20:12:55 +0200

    show information and news on front page

--- site/wp-content/themes/rff/front-page.php
+++ site/wp-content/themes/rff/front-page.php
@@ -1,9 +1,50 @@
 <?php get_header(); ?>
 
-<?php while ( have_posts() ) : the_post(); ?>
+<div class="row">
 
-  <?php echo the_content(); ?>
+  <div class="col-xs-6">
+    <section class="frontpage-section">
+      <?php $category_informatii = get_category_by_slug('informatii'); ?>
+      <h2 class="frontpage-sectiontitle"><?= $category_informatii->name ?></h2>
 
-<?php endwhile; ?>
+      <?php
+      $query_informatii = new WP_Query(
+        'showposts=5&cat=' . $category_informatii->term_id);
+      while($query_informatii->have_posts()) {
+        $query_informatii->the_post(); ?>
+
+        <article class="frontpage-post">
+          <h3 class="frontpage-posttitle">
+            <?= get_the_title($query_stiri->post->ID) ?>
+          </h3>
+        </article>
+
+      <?php } wp_reset_postdata(); ?>
+    </section>
+  </div>
+
+
+  <div class="col-xs-6">
+    <section class="frontpage-section">
+      <?php $category_stiri = get_category_by_slug('stiri'); ?>
+      <h2 class="frontpage-sectiontitle"><?= $category_stiri->name ?></h2>
+
+      <?php
+      $query_stiri = new WP_Query(
+        'showposts=5&cat=' . $category_stiri->term_id);
+      while($query_stiri->have_posts()) {
+        $query_stiri->next_post(); ?>
+
+        <article class="frontpage-post">
+          <h3 class="frontpage-posttitle">
+            <?= get_the_title($query_stiri->post->ID) ?>
+          </h3>
+        </article>
+
+      <?php } wp_reset_postdata(); ?>
+    </section>
+  </div>
+
+</div>
 
 <?php get_footer(); ?>
