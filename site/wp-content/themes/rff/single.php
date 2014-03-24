7b1c82e312fdfdbf4863e497a56b6832e193c4da
Alex Morega <alex@grep.ro>
2014-03-24 16:47:11 +0200

    template for single post

---
+++ site/wp-content/themes/rff/single.php
@@ -0,0 +1,26 @@
+<?php
+get_header();
+
+the_post();
+?>
+
+<article class="single-post">
+  <h3 class="single-posttitle"><?= get_the_title() ?></h3>
+
+  <footer>
+    de <strong><?= get_the_author(); ?></strong>,
+    <?= get_the_date(); ?>
+  </footer>
+
+  <?php if(has_post_thumbnail()) {
+    $image = wp_get_attachment_image_src(
+      get_post_thumbnail_id(),
+      'single-post-thumbnail' );
+
+    echo '<img class="single-postthumbnail" src="' . $image[0] . '">';
+  } ?>
+
+  <? the_content(); ?>
+</article>
+
+<?php get_footer(); ?>
