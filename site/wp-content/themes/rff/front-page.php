a1fc15dff7c5588b162208750b8183b11559344a
Alex Morega <alex@grep.ro>
2014-03-24 14:11:41 +0200

    background for front-page news list

--- site/wp-content/themes/rff/front-page.php
+++ site/wp-content/themes/rff/front-page.php
@@ -3,10 +3,11 @@
 <div class="row">
 
   <div class="col-xs-6">
-    <section class="frontpage-section">
+    <section class="frontpage-section info-section">
       <?php $category_informatii = get_category_by_slug('informatii'); ?>
       <h2 class="frontpage-sectiontitle"><?= $category_informatii->name ?></h2>
 
+      <div class="frontpage-section-articles">
       <?php
       $query_informatii = new WP_Query(
         'showposts=5&cat=' . $category_informatii->term_id);
@@ -25,15 +26,17 @@
         </article>
 
       <?php } wp_reset_postdata(); ?>
+      </div>
     </section>
   </div>
 
 
   <div class="col-xs-6">
-    <section class="frontpage-section">
+    <section class="frontpage-section news-section">
       <?php $category_stiri = get_category_by_slug('stiri'); ?>
       <h2 class="frontpage-sectiontitle"><?= $category_stiri->name ?></h2>
 
+      <div class="frontpage-section-articles">
       <?php
       $query_stiri = new WP_Query(
         'showposts=5&cat=' . $category_stiri->term_id);
@@ -52,6 +55,7 @@
         </article>
 
       <?php } wp_reset_postdata(); ?>
+      </div>
     </section>
   </div>
 
